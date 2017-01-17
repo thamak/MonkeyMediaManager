2b2a7c8b2d1b1f5c72242b66b0e660da5ce1054e
thamak <thamakhoung@gmail.com>
2017-01-17 20:44:01 +0100

    Modifs

--- MonkeyMediaManager/Program.cs
+++ MonkeyMediaManager/Program.cs
@@ -1,31 +1,15 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 
 namespace MonkeyMediaManager
 {
-    public class Program
+    public static class WindowsApp
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .AddCommandLine(args)
-                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-                .Build();
-
-            var host = new WebHostBuilder()
-                .UseConfiguration(config)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
         }
     }
 }
